5d158f1c6e0979447c4fd2d19ffbad834dee29c2
denismwangi <denniske992@gmail.com>
2021-06-16 18:27:00 +0300

    modell #error

---
+++ src/main/java/com/aver/publisher/controller/MessageController.java
@@ -0,0 +1,34 @@
+package com.aver.publisher.controller;
+
+import com.aver.publisher.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    private String topic = "kafka-prod";
+
+
+    @GetMapping("/publish/{name}")
+    public String publishMessage(@PathVariable String name){
+
+        template.send(topic,"hello" +name+ "kafka is awesome");
+        return "Data published";
+    }
+
+    @GetMapping("/publishJson")
+    public String publishMessage(){
+        User user = new User(1, "denis","4567890");
+        template.send(topic, user);
+
+        return "Json data published";
+    }
+
+}
---
+++ src/main/java/com/aver/publisher/model/User.java
@@ -0,0 +1,18 @@
+package com.aver.publisher.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private int Id;
+    private String name;
+    private String address;
+
+
+}
